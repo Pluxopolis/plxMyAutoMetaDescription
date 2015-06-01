a0eddbd9e17feb1d54e4246f7664c03276bcd519
Stephane <stephane@pluxml.org>
2015-06-01 14:45:47 +0200

    Compatibilité PluXml 5.4

--- config.php
+++ config.php
@@ -14,14 +14,12 @@ $nbwords = $plxPlugin->getParam('nbwords')!='' ? $plxPlugin->getParam('nbwords')
 
 ?>
 
-<h2><?php echo $plxPlugin->getInfo('title') ?></h2>
-
 <form action="parametres_plugin.php?p=plxMyAutoMetaDescription" method="post" id="form_plxMyAutoMetaDescription">
 	<fieldset>
 		<?php $plxPlugin->lang('L_NBWORDS_HELP') ?>
 		<p class="field"><label for="id_nbwords"><?php $plxPlugin->lang('L_NBWORDS') ?></label></p>
 		<?php plxUtils::printInput('nbwords',$nbwords,'text','4-4') ?>
-		<p>
+		<p class="in-action-bar">
 			<?php echo plxToken::getTokenPostMethod() ?>
 			<input type="submit" name="submit" value="<?php $plxPlugin->lang('L_SAVE') ?>" />
 		</p>
