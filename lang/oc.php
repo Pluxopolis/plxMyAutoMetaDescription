3b205fc7ee10ae945aa790e0b5bbc2182632a1df
Quent-in <pages_quentin@hotmail.com>
2016-04-20 19:45:12 +0200

    Occitan version

---
+++ lang/oc.php
@@ -0,0 +1,8 @@
+<?php
+$LANG = array(
+# config.php
+'L_NBWORDS'				=> 'Nombre de paraulas a utilizar',
+'L_NBWORDS_HELP'		=> 'Se lo camp meta descripcion de l\'article es pas emplenat, lo tèxte del chapò e lo contengut de l\'article seràn utilizats per l\'emplenar automaticament.',
+'L_SAVE'				=> 'Enregistrar',
+);
+?>
